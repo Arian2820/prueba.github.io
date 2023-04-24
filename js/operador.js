efc899516851b1dc9feaf8d042ffe59014831b0e
Arian2820 <129077240+Arian2820@users.noreply.github.com>
2023-04-24 09:34:01 -0500

    main

--- js/operador.js
+++ js/operador.js
@@ -3,7 +3,7 @@ var celsuis1 = prompt("Introduzca la temperatura en grados celsius: ");
 celsuis1= parseFloat(celsuis1);
 // conversion corta:var celsuis1 = promptFloat("Introduzca la temperatura en grados celsius: "))
 //procedo a hacer la conversion de celsius a farenheit1:
-var farenheit1=(9/5)*celsius1+32;
+var farenheit1=(9/5)*celsuis1+32;
 
 document.write("De celsius a Farenheit es", farenheit1);
 
