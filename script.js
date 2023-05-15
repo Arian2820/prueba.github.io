ae3e7a72ea1ca5212acbdb6682a5450a50ee2be9
Arian2820 <129077240+Arian2820@users.noreply.github.com>
2023-05-15 09:30:17 -0500

    main

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var anio=prompt("Cual es su año de nacimiento?");
 anio= parsefloat (anio);
 var edad=(2023)-anio;
-documet.write("La edad del usuario es:"anio)
+documet.write("La edad del usuario es:",anio)
